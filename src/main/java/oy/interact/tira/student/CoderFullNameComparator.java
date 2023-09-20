77bda025105b33069cf5764ebdba12dab7ceea06
Johan Sääskilahti <112552334+saaskilahtij@users.noreply.github.com>
2023-09-20 12:56:11 +0300

    Added CoderNameComparator.java and comparator

--- src/main/java/oy/interact/tira/student/CoderFullNameComparator.java
+++ src/main/java/oy/interact/tira/student/CoderFullNameComparator.java
@@ -7,6 +7,9 @@ public class CoderFullNameComparator implements Comparator<Coder>{
   @Override
   public int compare(Coder c1, Coder c2) {
 
+    // Compares coder full names and returns 1 if c1 full name comes first
+    // and -1 if c2 full name comes first
+    // If the full names are the same returns 0
     if (c1.getLastName().compareTo(c2.getLastName()) < 0) {
       return 1;
     } else if (c1.getLastName().compareTo(c2.getLastName()) > 0) {
---
+++ src/main/java/oy/interact/tira/student/CoderNameComparator.java
@@ -0,0 +1,19 @@
+package oy.interact.tira.student;
+import oy.interact.tira.model.Coder;
+import java.util.Comparator;
+
+public class CoderNameComparator implements Comparator<Coder> {
+
+  // Compares coder names and returns 1 if c1 name comes first
+  // and -1 if c2 name comes first
+  // If the names are the same returns 0
+  @Override
+  public int compare(Coder c1, Coder c2) {
+    if (c1.getCoderName().compareTo(c2.getCoderName()) < 0) {
+      return 1;
+    } else if (c1.getCoderName().compareTo(c2.getCoderName()) > 0) {
+      return -1;
+    }
+    return 0;
+  }
+}
