6d8f2715c0423a69c2563ac88f41ee200c6b4d86
Johan Sääskilahti <112552334+saaskilahtij@users.noreply.github.com>
2023-09-20 12:45:00 +0300

    CoderFullNameComparator.java comparator now works!

--- src/main/java/oy/interact/tira/student/CoderFullNameComparator.java
+++ src/main/java/oy/interact/tira/student/CoderFullNameComparator.java
@@ -2,16 +2,23 @@ package oy.interact.tira.student;
 import oy.interact.tira.model.Coder;
 import java.util.Comparator;
 
-
 public class CoderFullNameComparator implements Comparator<Coder>{
 
   @Override
   public int compare(Coder c1, Coder c2) {
-    if (c1.compareTo(c2) < 0) {
-      return -1;
-    } else if (c1.compareTo(c2) > 0) {
+
+    if (c1.getLastName().compareTo(c2.getLastName()) < 0) {
       return 1;
+    } else if (c1.getLastName().compareTo(c2.getLastName()) > 0) {
+      return -1;
+    } else {
+      if (c1.getFirstName().compareTo(c2.getFirstName()) < 0) {
+        return 1;
+      } else if (c1.getFirstName().compareTo(c2.getFirstName()) > 0) {
+        return -1;
+      } else {
+        return 0;
+      }
     }
-    return 0;
   }
 }
